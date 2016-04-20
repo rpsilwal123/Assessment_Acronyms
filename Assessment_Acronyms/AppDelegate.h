0c35afda091b78a17addaf596b85f6eb33b6b4b4
Ranjan <ranjans.bss@gmail.com>
2016-04-19 23:27:37 -0700

    Initial Commit

---
+++ Assessment_Acronyms/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Assessment_Acronyms
+//
+//  Created by Ranjan on 4/19/16.
+//  Copyright © 2016 Ranjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Assessment_Acronyms/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Assessment_Acronyms
+//
+//  Created by Ranjan on 4/19/16.
+//  Copyright © 2016 Ranjan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
